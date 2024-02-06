9a62f1f787e81c50b1206633e8e5603bb434369d
admin <admin@DESKTOP-2PV3CAH>
2024-02-06 09:31:24 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ETicketsApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
